bcc83b5f5f757e58f0b79e837d1050ecd3374ff8
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-01-03 22:26:09 +0600

    show data in server

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+// ..........................................................
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvn5rcy.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,30 +27,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    // create database
-    const menuCollection = client.db("restaurantDb").collection("menu");
-    const reviewCollection = client.db("restaurantDb").collection("reviews");
-    const cartCollection = client.db("restaurantDb").collection("carts");
+    // ....................................................
 
-    // to get data from server -> for menu
+    const menuCollection = client.db("bistroDb").collection("menu");
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
-    // to get data from server -> for reviews
-    app.get("/reviews", async (req, res) => {
-      const result = await reviewCollection.find().toArray();
-      res.send(result);
-    });
 
-    // when we hit it will start
-    // carts collection
-    app.post("/carts", async (req, res) => {
-      const cartItem = req.body;
-      const result = await cartCollection.insertOne(cartItem);
-      res.send(result);
-    });
-
+    // ....................................................
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -61,6 +48,7 @@ async function run() {
 }
 run().catch(console.dir);
 
+// ..........................................................
 app.get("/", (req, res) => {
   res.send("Boss is running");
 });
